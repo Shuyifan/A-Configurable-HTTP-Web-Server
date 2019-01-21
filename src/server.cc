86bf62c6adaba3357e8766cd1f9e19f42f880e93
yifanshu <yifanshu@cs130.org>
2019-01-21 10:46:35 -0800

    make some minor change
    Change-Id: I6b9e269fff23e03803edcdd142f51a800111e83a

--- src/server.cc
+++ src/server.cc
@@ -15,12 +15,12 @@ server::server(boost::asio::io_service& io_service, short port)
 void server::start_accept() {
     session* new_session = new session(io_service_);
     acceptor_.async_accept(new_session->socket(),
-        boost::bind(&server::handle_accept, this, new_session,
-          boost::asio::placeholders::error));
+                           boost::bind(&server::handle_accept, this, new_session,
+                           boost::asio::placeholders::error));
 }
 
 void server::handle_accept(session* new_session,
-      const boost::system::error_code& error) {
+                           const boost::system::error_code& error) {
 
     if(!error) {
       new_session->start();
--- src/session.cc
+++ src/session.cc
@@ -15,19 +15,19 @@ tcp::socket& session::socket() {
 
 void session::start() {
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_read, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
+                            boost::bind(&session::handle_read, this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
 }
 
 void session::handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred) {
+                          size_t bytes_transferred) {
     if(!error) {
         std::string response_ = get_response();
         boost::asio::async_write(socket_,
-          boost::asio::buffer(response_),
-          boost::bind(&session::handle_write, this,
-            boost::asio::placeholders::error));
+                                 boost::asio::buffer(response_),
+                                 boost::bind(&session::handle_write, this,
+                                 boost::asio::placeholders::error));
     } else {
         delete this;
     }
@@ -36,9 +36,9 @@ void session::handle_read(const boost::system::error_code& error,
 void session::handle_write(const boost::system::error_code& error) {
     if(!error) {
         socket_.async_read_some(boost::asio::buffer(data_, max_length),
-          boost::bind(&session::handle_read, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+                                boost::bind(&session::handle_read, this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
     } else {
       delete this;
     }
