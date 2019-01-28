d2be1b20315bd177fb8793c1d94be81cd6619859
Qiwen Lyu <qlyu001@cs130.org>
2019-01-28 07:43:10 +0000

    integration test down
    Change-Id: I69678e4fa77cefd03f345483f1fd92e5c31544d2

--- tests/intergration_test.sh
+++ tests/intergration_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-STRING1=`echo -e "hello" | nc localhost 80`
-STRING2=$'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nhello'
+INPUTC=`echo -e "GET / HTTP/1.1\r\n\r\nxxxxx\r\n\r\n" | nc localhost 8080 -w 5`
+OUTPUTC=$'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nGET / HTTP/1.1\r\n\r\nxxxxx\r\n\r\n'
+OUTPUTC=${OUTPUTC%$'\n'}
 
-
-if [ "$STRING1" == "$STRING2" ]; then
+if [ "$INPUTC" == "$OUTPUTC" ]; then
     echo "pass"
 else
     echo "fail"
+    echo "$INPUTC" | od -c
+    echo "$OUTPUTC" | od -c
 fi
