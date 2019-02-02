a2594eca093a5cadfdaffe80718973046f1a4b69
Apple <chengshun@cs130.org>
2019-02-02 08:05:20 +0000

    make the code style consistent
    Change-Id: Ie6472b8187ac2aa60ead7835bea5c929d460c280

--- include/static_handler.h
+++ include/static_handler.h
@@ -8,16 +8,18 @@ namespace http {
 namespace server {
 class StaticHandler : public RequestHandler {
 public:
- StaticHandler() {}
- virtual bool handleRequest(const request& req, std::string& response) override;
-   /// Perform URL-decoding on a string. Returns false if the encoding was
-  /// invalid.
-static bool url_decode(const std::string& in, std::string& out);
-/// Construct with a directory containing files to be served.
-explicit StaticHandler(const std::string& doc_root);
+	StaticHandler() {}
+	/// Construct with a directory containing files to be served.
+	explicit StaticHandler(const std::string& doc_root);
+	
+	virtual bool handleRequest(const request& req, std::string& response) override;
+	/// Perform URL-decoding on a string. Returns false if the encoding was
+	/// invalid.
+	static bool url_decode(const std::string& in, std::string& out);
+	
 private:
-  /// The directory containing the files to be served.
-  std::string doc_root_;
+	/// The directory containing the files to be served.
+	std::string doc_root_;
 };
 }
 }
--- src/static_handler.cc
+++ src/static_handler.cc
@@ -15,49 +15,44 @@
 namespace http {
 namespace server {
 StaticHandler::StaticHandler(const std::string& doc_root)
-  : doc_root_(doc_root)
-{
-}
- bool StaticHandler::handleRequest(const request& req, std::string& response) {
+  : doc_root_(doc_root) {}
+
+bool StaticHandler::handleRequest(const request& req, std::string& response) {
     std::stringstream res;
 
     // Decode url to path.
     std::string request_path;
-    if (!url_decode(req.uri, request_path))
-    {
+    if(!url_decode(req.uri, request_path)) {
       //TODO:make the response as bad request
       //rep = reply::stock_reply(reply::bad_request);
       return false;
     }
     // Request path must be absolute and not contain "..".
-    if (request_path.empty() || request_path[0] != '/'|| request_path.find("..") != std::string::npos)
-    {
+    if(request_path.empty() || request_path[0] != '/'|| request_path.find("..") != std::string::npos) {
       //TODO:make the response as bad request
       //rep = reply::stock_reply(reply::bad_request);
       return false;
     }
     
 
-  // Determine the file extension.
-  std::size_t last_slash_pos = request_path.find_last_of("/");
-  std::size_t last_dot_pos = request_path.find_last_of(".");
-  std::string extension;
-  if (last_dot_pos != std::string::npos && last_dot_pos > last_slash_pos)
-  {
-    extension = request_path.substr(last_dot_pos + 1);
-  }
+	// Determine the file extension.
+	std::size_t last_slash_pos = request_path.find_last_of("/");
+	std::size_t last_dot_pos = request_path.find_last_of(".");
+	std::string extension;
+	if(last_dot_pos != std::string::npos && last_dot_pos > last_slash_pos) {
+		extension = request_path.substr(last_dot_pos + 1);
+	}
 
-  // Open the file to send back.
-  std::string full_path = doc_root_ + request_path;
-  std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
-  if (!is)
-  {
-    //TODO:make the response as bad request
-    //rep = reply::stock_reply(reply::not_found);
-    return false;
-  }
-  char buf[512];
-  while (is.read(buf, sizeof(buf)).gcount() > 0)
+	// Open the file to send back.
+	std::string full_path = doc_root_ + request_path;
+	std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
+	if(!is) {
+		//TODO:make the response as bad request
+		//rep = reply::stock_reply(reply::not_found);
+		return false;
+	}
+	char buf[512];
+	while (is.read(buf, sizeof(buf)).gcount() > 0)
   
     res << "Content-Length";
     //res << std::to_string(rep.content.size());
@@ -77,43 +72,30 @@ StaticHandler::StaticHandler(const std::string& doc_root)
     response = res.str();
 }
 
-bool StaticHandler::url_decode(const std::string& in, std::string& out)
-{
-  out.clear();
-  out.reserve(in.size());
-  for (std::size_t i = 0; i < in.size(); ++i)
-  {
-    if (in[i] == '%')
-    {
-      if (i + 3 <= in.size())
-      {
-        int value = 0;
-        std::istringstream is(in.substr(i + 1, 2));
-        if (is >> std::hex >> value)
-        {
-          out += static_cast<char>(value);
-          i += 2;
-        }
-        else
-        {
-          return false;
-        }
-      }
-      else
-      {
-        return false;
-      }
-    }
-    else if (in[i] == '+')
-    {
-      out += ' ';
-    }
-    else
-    {
-      out += in[i];
-    }
-  }
-  return true;
+bool StaticHandler::url_decode(const std::string& in, std::string& out) {
+	out.clear();
+	out.reserve(in.size());
+	for(std::size_t i = 0; i < in.size(); ++i) {
+		if(in[i] == '%') {
+			if(i + 3 <= in.size()) {
+				int value = 0;
+				std::istringstream is(in.substr(i + 1, 2));
+				if (is >> std::hex >> value) {
+					out += static_cast<char>(value);
+					i += 2;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else if(in[i] == '+') {
+			out += ' ';
+		} else {
+			out += in[i];
+		}
+	}
+  	return true;
 }
 
 
