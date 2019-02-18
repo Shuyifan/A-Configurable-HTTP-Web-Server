627273827adcd49ccd46d93471e2fb9ca54a142c
yifanshu <yifanshu@cs130.org>
2019-02-17 22:18:58 -0800

    add unit test for error handler
    Change-Id: I3dafa9fbd44599d9e2f808228e588eb65ecce370

--- src/error_handler.cc
+++ src/error_handler.cc
@@ -17,7 +17,7 @@ std::unique_ptr<http::server::Response> ErrorHandler::HandlerRequest(const reque
     std::unique_ptr<Response> response_ (new Response);
     response_->SetStatus(Response::not_found);
     response_->SetVersion("1.1");
-    response_->AddHeader("Content-type", "text/html");
+    response_->AddHeader("Content-Type", "text/html");
     response_->SetContent("<h1>404: Page Not Found</h1>");
     return response_;
 }
---
+++ tests/error_handler_test.cc
@@ -0,0 +1,24 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "request.h"
+#include "error_handler.h"
+
+class ErrorHandlerTest : public ::testing::Test {
+  protected:
+    http::server::request request_;
+    http::server::RequestHandler* requestHandler = new http::server::ErrorHandler();
+};
+
+TEST_F(ErrorHandlerTest, errorHandlerTest) {
+    std::unique_ptr<http::server::Response> response_;
+    request_.method = "GET";
+    request_.uri = "/";
+    request_.http_version_major = 1;
+    request_.http_version_minor = 1;
+    response_ = requestHandler->HandlerRequest(request_);
+    std::string res = response_->ToString();
+    std::cout << "******************" << std::endl;
+    std::cout << res << std::endl;std::cout << "******************" << std::endl;
+    EXPECT_EQ(res, "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"\
+                   "<h1>404: Page Not Found</h1>");
+}
