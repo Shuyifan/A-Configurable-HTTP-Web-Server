2ae7598e24f2a376f62bec14b7eed82de3a0b14b
王雨晴 <yuqingw@cs130.org>
2019-03-11 06:57:56 +0000

    search handler test skeleton and basic code
    Change-Id: Ia2965a70b397e1b56142372b5fef04bf260a4cae

---
+++ tests/search_handler_test.cc
@@ -0,0 +1,37 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "handler_manager.h"
+
+class SearchHandlerTest : public ::testing::Test {
+protected:
+
+    void SetUp() {
+        request.method = "GET";
+        request.uri = "/meme/search?q=hi";
+        request.http_version_major = 1;
+        request.http_version_minor = 1;
+        parser.Parse("test.conf", &config);
+        manager = new http::server::HandlerManager(config);
+        handler = manager->createByUrl("/meme/search");
+        response = handler->HandlerRequest(request);
+        resStr = response->ToString();
+    }
+
+    std::unique_ptr<http::server::RequestHandler> handler;
+    NginxConfig config;
+    NginxConfigParser parser;
+    http::server::HandlerManager* manager;
+    http::server::request request;
+    std::unique_ptr<http::server::Response> response;
+    std::string resStr;
+};
+
+TEST_F(SearchHandlerTest, ResponseOK) {
+    EXPECT_EQ(resStr.substr(0, 15), "HTTP/1.1 200 OK");
+}
+
+TEST_F(SearchHandlerTest, responseEmptyTest) {
+    std::stringstream ss;
+    ss << "";
+    EXPECT_TRUE(resStr.find(ss.str()) != resStr.npos);
+}
